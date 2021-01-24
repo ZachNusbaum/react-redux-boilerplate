8763052c1b862fbc08e44ee8286f2291f8e5768b
Zachary Nusbaum <zach@zdn.dev>
2021-01-24 12:30:47 -0800

    Can mark todos as completed

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {removeTodo} from "./actions";
+import {removeTodo, markTodoAsCompleted} from "./actions";
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
-const TodoList = ({todos = [{text: "Hello"}], onRemovePressed}) => (
+const TodoList = ({todos = [{text: "Hello"}], onRemovePressed, onCompletedPressed}) => (
     <div className="list-wrapper">
         <NewTodoForm />
-        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
+        {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} onCompletedPressed={onCompletedPressed} />)}
     </div>
 )
 
@@ -17,7 +17,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-    onRemovePressed: text => dispatch(removeTodo(text))
+    onRemovePressed: text => dispatch(removeTodo(text)),
+    onCompletedPressed: text => dispatch(markTodoAsCompleted(text))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
